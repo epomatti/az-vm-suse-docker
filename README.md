de450d09fbedb3fd0448fe6f622a1308eb1ab978
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-15 18:06:51 -0300

    compose

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 ```sh
 ssh-keygen -f modules/suse/id_rsa
 ```
+
+
+
+```
+ssh -i modules/suse/id_rsa suseadmin@....
+```
+
+https://docs.docker.com/compose/install/standalone/
+https://scc.suse.com/packages?name=SUSE%20Linux%20Enterprise%20Server&version=12.5&arch=x86_64&query=docker-compose&module=
--- modules/suse/userdata.sh
+++ modules/suse/userdata.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 sudo zypper refresh
-sudo SUSEConnect -p sle-module-containers/12/x86_64 -r ''
 
 # Azure CLI
 sudo zypper install -y azure-cli
 
 # Docker
+sudo SUSEConnect -p sle-module-containers/12/x86_64 -r ''
 sudo zypper install -y docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
+# Docker Compose
+# SUSEConnect -p PackageHub/12.5/x86_64
+# zypper install docker-compose
+
+# Compose
+curl -SL https://github.com/docker/compose/releases/download/v2.21.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
