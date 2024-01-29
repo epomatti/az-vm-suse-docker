c357a562a157925083c607adc868dc408b9ba69e
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-01-29 20:35:34 -0300

    bump azurerm

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
+      source  = "hashicorp/azurerm"
+      version = "3.89.0"
     }
   }
 }
